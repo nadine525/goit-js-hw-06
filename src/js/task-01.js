7a911499dae173d8767d45411099123959dadc19
nadine525 <02_seeders_wide@icloud.com>
2023-03-19 16:19:07 +0100

    task-02

--- src/js/task-01.js
+++ src/js/task-01.js
@@ -1,10 +1,9 @@
 const allCategories = document.querySelectorAll(".item");
 console.log(`Number of categories: ${allCategories.length}`);
 
-const Categories = document.querySelectorAll(".item");
-Categories.forEach((item) => {
+allCategories.forEach((item) => {
   const nameOfCategory = item.querySelector("h2");
   console.log(`Category: ${nameOfCategory.textContent}`);
-  const valueElements = item.querySelector("ul");
-  console.log(`Elements: ${valueElements.querySelectorAll("li").length}`);
+  const quantityOfElements = item.querySelector("ul");
+  console.log(`Elements: ${quantityOfElements.querySelectorAll("li").length}`);
 });
--- src/js/task-02.js
+++ src/js/task-02.js
@@ -1,8 +1,24 @@
 const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
+  "Potatoes",
+  "Mushrooms",
+  "Garlic",
+  "Tomatos",
+  "Herbs",
+  "Condiments",
 ];
+
+const items = [];
+
+for (let i = 0; i < ingredients.length; i += 1) {
+  const food = ingredients[i];
+
+  const liElement = document.createElement("li");
+  liElement.textContent = food;
+  liElement.classList.add("li");
+  items.push(liElement);
+}
+
+const list = document.querySelector("#ingredients");
+list.append(...items);
+
+console.log(list);
