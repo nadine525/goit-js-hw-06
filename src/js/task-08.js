bcf326ff1826123bea1693e193fb688ba9136d43
nadine525 <02_seeders_wide@icloud.com>
2023-03-22 11:50:35 +0100

    Update task-08.js

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,21 @@
+const loginForm = document.querySelector(".login-form");
 
+const onFormSubmit = (event) => {
+  event.preventDefault();
+
+  const { email, password } = event.currentTarget;
+
+  if (email.value === "" || password.value === "") {
+    return alert("всі поля повинні бути заповнені");
+  }
+  const userData = {
+    email: email.value,
+    password: password.value,
+  };
+
+  console.log(userData);
+
+  event.currentTarget.reset();
+};
+
+loginForm.addEventListener("submit", onFormSubmit);
